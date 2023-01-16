1777877f9a634d769129b44a00e77757a311554c
thedingleberry <cyril.redmond@gmail.com>
2023-01-16 15:02:53 +0800

    Initial commit

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('templates/index.html')
-    
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
